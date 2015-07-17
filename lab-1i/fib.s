3e09737a6f4ab96469fd6cb043d7178e9fde7272
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-17 21:40:43 +0800

    lab-1i: Implement fibonacci with loop
    The original c source is comment in fibseq.c.
    
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,32 +11,50 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, r4, r5, r6, lr}
 
 	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	sub r4, r0, #0
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	@ if(R0 <=1 ) goto .L3 (which returns R0)
+	cmp r0, #1
+	ble .L3
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	@ store fibo to r3 and fibo = 1
+	mov r3, #1
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	@ store fiboPrev to r4 and fiboPrev = 1
+	mov r4, #1
 
-	@ R0 = R5 + R0 (update flags)
+	@ for loop, store i to r6 and i = 2
+	mov r6, #2
 
-	pop {r3, r4, r5, pc}		@EPILOG
+.loop:
+
+	@ r5 = tmp = fibo
+	mov r5, r3
+
+	@ fibo += fiboPrev (r3 += r4)
+	add r3, r4
+
+	@ fiboPrev = tmp (r4 = r5)
+	mov r4, r5
+
+	@ i++
+	add r6, #1
+
+	@ if (r6 < r0) goto .loop
+	cmp r6, r0
+	blt .loop
+
+	@ store result to r0
+	mov r0, r3
+
+	pop {r3, r4, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
 	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -2,13 +2,30 @@
 
 extern int fibonacci(int x);
 
+# if 0
+int fibonacci(int n)
+{
+  if(n <= 1){
+    return n;
+  }
+  int fibo = 1;      // R3
+  int fiboPrev = 1;  // R4
+  int tmp = 0;       // R5
+  for(int i = 2; i < n; i++){ // R6
+    tmp = fibo;
+    fibo += fiboPrev;
+    fiboPrev = tmp;
+  }
+  return fibo;
+}
+#endif
+
 int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
 
   scanf("%d",&number);
-  result = fibonacci(number);   
+  result = fibonacci(number);
   printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
-
